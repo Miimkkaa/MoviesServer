59c30ab221dd0ec45de84adbb6b9567bf8b5e231
pshemkooo <pshemko1993@outlook.dk>
2020-04-21 13:48:09 +0200

    new

--- app/controllers/favorites.controller.js
+++ app/controllers/favorites.controller.js
@@ -1,6 +1,6 @@
 const db = require("../models");
 
-const Favorites = db.Favorites;
+const Favorites = db.favorites;
 
 const Op = db.Sequelize.Op;
 
@@ -74,10 +74,6 @@ exports.findAll = (req, res) => {
 
     var condition = userid ? { userid: { [Op.eq]: `%${userid}%` } } : null; 
 
-    // [Op.eq]: '%' + (userid) + '%'
-  
-  
-
     Favorites.findAll({ where: condition })
 
       .then(data => {
@@ -103,4 +99,203 @@ exports.findAll = (req, res) => {
 };
 
 
+// Find a single Tutorial with an id
 
+exports.findOne = (req, res) => {
+
+  const id = req.params.userid;
+
+
+
+  Favorites.findByPk(id)
+
+    .then(data => {
+
+      res.send(data);
+
+    })
+
+    .catch(err => {
+
+      res.status(500).send({
+
+        message: "Error retrieving Tutorial with id=" + id
+
+      });
+
+    });
+
+
+
+};
+
+
+
+
+// Update a Tutorial by the id in the request
+
+exports.update = (req, res) => {
+
+  const id = req.params.id;
+
+
+
+  Favorites.update(req.body, {
+
+    where: { id: id }
+
+  })
+
+    .then(num => {
+
+      if (num == 1) {
+
+        res.send({
+
+          message: "Tutorial was updated successfully."
+
+        });
+
+      } else {
+
+        res.send({
+
+          message: `Cannot update Tutorial with id=${id}. Maybe Tutorial was not found or req.body is empty!`
+
+        });
+
+      }
+
+    })
+
+    .catch(err => {
+
+      res.status(500).send({
+
+        message: "Error updating Tutorial with id=" + id
+
+      });
+
+    });
+
+
+
+};
+
+
+
+// Delete a Tutorial with the specified id in the request
+
+exports.delete = (req, res) => {
+
+  const id = req.params.id;
+
+
+
+  Favorites.destroy({
+
+    where: { id: id }
+
+  })
+
+    .then(num => {
+
+      if (num == 1) {
+
+        res.send({
+
+          message: "Tutorial was deleted successfully!"
+
+        });
+
+      } else {
+
+        res.send({
+
+          message: `Cannot delete Tutorial with id=${id}. Maybe Tutorial was not found!`
+
+        });
+
+      }
+
+    })
+
+    .catch(err => {
+
+      res.status(500).send({
+
+        message: "Could not delete Tutorial with id=" + id
+
+      });
+
+    });
+
+
+
+};
+
+
+
+// Delete all Tutorials from the database.
+
+exports.deleteAll = (req, res) => {
+
+  Favorites.destroy({
+
+      where: {},
+
+      truncate: false
+
+    })
+
+      .then(nums => {
+
+        res.send({ message: `${nums} Tutorials were deleted successfully!` });
+
+      })
+
+      .catch(err => {
+
+        res.status(500).send({
+
+          message:
+
+            err.message || "Some error occurred while removing all tutorials."
+
+        });
+
+      });
+
+  
+
+};
+
+
+
+// Find all published Tutorials
+
+exports.findAllPublished = (req, res) => {
+
+  Favorites.findAll({ where: { published: true } })
+
+  .then(data => {
+
+    res.send(data);
+
+  })
+
+  .catch(err => {
+
+    res.status(500).send({
+
+      message:
+
+        err.message || "Some error occurred while retrieving tutorials."
+
+    });
+
+  });
+
+
+
+};
--- app/routes/favorites.routes.js
+++ app/routes/favorites.routes.js
@@ -19,9 +19,31 @@ module.exports = app => {
     router.get("/", favorites.findAll);
 
     
+  // Retrieve all published Tutorials
+
+  router.get("/published", favorites.findAllPublished);
+
+
+  // Retrieve a single Tutorial with id
+
+  router.get("/:id", favorites.findOne);
+
+
+
+  // Update a Tutorial with id
+
+  router.put("/:id", favorites.update);
+
+
+  // Delete a Tutorial with id
+
+  router.delete("/:id", favorites.delete);
+
+
+  // Create a new Tutorial
+
+  router.delete("/", favorites.deleteAll);
    
-  
-  
     app.use('/api/favorites', router);
   
   };
