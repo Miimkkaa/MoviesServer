7e113a37246054371cd0496009fde3712524e5f1
Marek Laslo <mare1168@edu.easj.dk>
2020-04-22 14:31:57 +0200

    CRUD

--- app/models/index.js
+++ app/models/index.js
@@ -1,5 +1,5 @@
 const Sequelize = require("sequelize");
-const sequelize = new Sequelize('MovieDB', 'root', '', {
+const sequelize = new Sequelize('movieDB', 'root', 'e@syLook71', {
   host: "localhost",
   dialect: "mysql",
   port: 3306
@@ -10,6 +10,7 @@ const db = {};
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 
-db.tutorials = require("./tutorial.model.js")(sequelize, Sequelize);
+//db.tutorials = require("./tutorial.model.js")(sequelize, Sequelize);
+db.movies = require("./movie.model.js")(sequelize, Sequelize);
 
 module.exports = db;
